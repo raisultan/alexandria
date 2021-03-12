4c32eb47073a86616172b85ebe620219a5abd806
raisultan <ki.xbozz@gmail.com>
2021-03-12 17:42:36 +0700

    http server added

--- golang/main.go
+++ golang/main.go
@@ -2,36 +2,13 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
+	"net/http"
 
 	"gopkg.in/yaml.v3"
 )
-
-type Action struct {
-	Description string
-	Actor       struct {
-		ObjectType string `yaml:"type"`
-		Properties struct {
-			IsSystem    bool
-			Description string
-		}
-	}
-	Data            map[string]interface{}
-	ResponseToGroup map[string]interface{} `yaml:"response_to_group"`
-	ResponseToUser  map[string]interface{} `yaml:"response_to_user"`
-}
-
-func (a Action) print() {
-	fmt.Printf(
-		"Description: %s\nActor is system: %t\nActor description: %s\nResponse to user event name: %s\nResponse to group event name: %s\n\n",
-		a.Description,
-		a.Actor.Properties.IsSystem,
-		a.Actor.Properties.Description,
-		a.ResponseToUser["event"],
-		a.ResponseToGroup["event"],
-	)
-}
 
 func main() {
 	ymlContent, err := ioutil.ReadFile("../examples/chat-ws.yaml")
@@ -45,8 +22,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for key, value := range actions {
-		fmt.Println("Action: ", key)
-		value.print()
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("./ws-documentation.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(w, actions)
+	})
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
 }
