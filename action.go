029fce13622fb93991066d7addb385324195b141
raisultan <ki.xbozz@gmail.com>
2021-03-13 17:26:33 +0700

    basic settings added to struct

--- action.go
+++ action.go
@@ -9,6 +9,23 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type RawDocumentation struct {
+	Info    Info
+	Server  WSServer
+	Actions map[string]RawAction
+}
+
+type Info struct {
+	Title       string
+	Description string
+	Version     string
+}
+
+type WSServer struct {
+	Url         string
+	Description string
+}
+
 type RawAction struct {
 	Description string
 	Actor       struct {
@@ -22,6 +39,27 @@ type RawAction struct {
 
 	ResponseToGroupRaw map[string]interface{} `yaml:"response_to_group"`
 	ResponseToUserRaw  map[string]interface{} `yaml:"response_to_user"`
+}
+
+type Documentation struct {
+	Info    Info
+	Server  WSServer
+	Actions []Action
+}
+
+func (d Documentation) print() {
+	fmt.Printf(
+		"Info title: %s\nInfo description: %s\nInfo version: %s\nServer url: %s\nServer description: %s\n\n",
+		d.Info.Title,
+		d.Info.Description,
+		d.Info.Version,
+		d.Server.Url,
+		d.Server.Description,
+	)
+
+	for _, action := range d.Actions {
+		action.print()
+	}
 }
 
 type Action struct {
@@ -73,29 +111,44 @@ func newAction(n string, r RawAction) Action {
 	return action
 }
 
-func getActionsFromFile(path string) []Action {
-	rawActions := getRawActionsFromFile(path)
+func getDocumentationFromFile(path string) Documentation {
+	rd := getRawDocumentationFromFile(path)
+	info := Info{
+		Title:       rd.Info.Title,
+		Description: rd.Info.Description,
+		Version:     rd.Info.Version,
+	}
+	server := WSServer{
+		Url:         rd.Server.Url,
+		Description: rd.Server.Description,
+	}
 	actions := []Action{}
 
-	for name, action := range rawActions {
+	fmt.Println(rd.Actions)
+
+	for name, action := range rd.Actions {
 		actions = append(actions, newAction(name, action))
 	}
 
-	return actions
+	return Documentation{
+		Info:    info,
+		Server:  server,
+		Actions: actions,
+	}
 }
 
-func getRawActionsFromFile(path string) map[string]RawAction {
+func getRawDocumentationFromFile(path string) RawDocumentation {
 	ymlContent, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rawActions := make(map[string]RawAction)
-	if err := yaml.Unmarshal([]byte(ymlContent), &rawActions); err != nil {
+	rawDocumentation := RawDocumentation{}
+	if err := yaml.Unmarshal([]byte(ymlContent), &rawDocumentation); err != nil {
 		log.Fatal(err)
 	}
 
-	return rawActions
+	return rawDocumentation
 }
 
 func composeSchema(s map[string]interface{}) string {
